e2158aee560b1dd3dc59bc043eb6824b06a074f7
dilicosta <dilicosta@gmail.com>
2015-07-09 17:39:33 -0300

    Inclusão do Aplicativo com ActionBar (Menu Superior)

---
+++ Menu/app/src/main/java/br/com/diego/menu/MainActivity.java
@@ -0,0 +1,46 @@
+package br.com.diego.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Infla o menu com os botoes do action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_search) {
+            mensagem("Clicou em Procurar");
+            return true;
+        } else if (id == R.id.action_refresh) {
+            mensagem("Clicou em Atualizar");
+            return true;
+        } else if (id == R.id.action_settings) {
+            mensagem("Clicou em Configurações");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void mensagem(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
